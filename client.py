dd13cabeb792cc7c8986903385a4b04dc2b3bf94
Amir <amir@musaev.online>
2020-10-20 19:14:24 +0000

    feat: Add connectors

--- client.py
+++ client.py
@@ -1,12 +1,46 @@
-import asyncio
-import websockets
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import websocket
+import json
 
 
-async def hello():
-    uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
-        await websocket.send("Hello world!")
-        msg = await websocket.recv()
-        print(msg)
+class ClientConnector:
+    def __init__(self):
+        self.ws = websocket.WebSocketApp("ws://localhost:8765", on_message=self.on_message)
+        self.routes = {}
 
-asyncio.get_event_loop().run_until_complete(hello())
+    def on_message(self, message):
+        message = json.loads(message)
+        if message['url'] and self.routes[message['url']]:
+            self.routes[message['url']]()
+
+    def send(self, url, msg):
+        self.ws.send(json.dumps({"url": url, "msg": msg}))
+
+    def run2(self):
+        thread.start_new_thread(lambda: self.ws.run_forever(), ())
+
+
+class ChatController:
+    def __init__(self, connector):
+        self.connector = connector
+
+    def list(self):
+        self.connector.send('/chats/list', [1, 2, 3])
+
+    def create(self):
+        self.connector.send('/chats/create', 'ok')
+
+
+websocket.enableTrace(True)
+c = ClientConnector()
+chatController = ChatController(c)
+c.routes = {
+    "/chats/list": chatController.list,
+    "/chats/create": chatController.create
+}
+c.run2()
+while True:
+    c.send('/chats/list', input())
--- server.py
+++ server.py
@@ -1,12 +1,30 @@
-import asyncio
-import websockets
+from abc import ABC
+import tornado.web
+import tornado.websocket
+import tornado.ioloop
+
+clients = []
 
 
-async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
+class ServerConnector(tornado.websocket.WebSocketHandler, ABC):
+    def open(self):
+        clients.append(self)
+        print("WebSocket opened")
 
-start_server = websockets.serve(echo, "localhost", 8765)
+    def on_message(self, message):
+        print(message)
+        for client in [c for c in clients if c != self]:
+            client.write_message(message)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+    def on_close(self):
+        clients.remove(self)
+        print("WebSocket closed")
+
+
+application = tornado.web.Application([(r"/", ServerConnector)])
+application.listen(8765, '127.0.0.1')
+try:
+    loop = tornado.ioloop.IOLoop.current()
+    loop.start()
+except KeyboardInterrupt:
+    tornado.ioloop.IOLoop.current().stop()
