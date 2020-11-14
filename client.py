337cf17eec0e8f498917557219928713a5221486
Amir <amir@musaev.online>
2020-11-14 08:27:25 +0000

    feat: Add response callback for client and server

--- client.py
+++ client.py
@@ -1,9 +1,15 @@
+import time
+from collections.abc import Callable
+from typing import Dict
+
 try:
     import thread
 except ImportError:
     import _thread as thread
 import websocket
 import json
+
+callbacks: Dict[int, Callable] = {}
 
 
 class ClientConnector:
@@ -14,12 +20,30 @@ class ClientConnector:
 
     def on_message(self, message):
         print(message)
-        message = json.loads(message)
-        if message['url'] and message['data'] and self.routes[message['url']]:
-            self.routes[message['url']](self, message['data'])
+        message: Dict = json.loads(message)
+        server_id = message.get('server_id')
+        client_id = message.get('client_id')
+        url = message.get('url')
+        if url and server_id:
+            handler = self.routes.get(url)
+            res = handler(self, message.get('data')) or None
+            self.ws.send(json.dumps({"server_id": server_id, "data": res}))
+        elif url:
+            handler = self.routes.get(url)
+            handler(self, message.get('data'))
+        elif client_id:
+            handler = callbacks.pop(client_id)
+            handler(self, message.get('data'))
 
-    def send(self, url, msg):
-        self.ws.send(json.dumps({"url": url, "data": msg}))
+    def send(self, url, message=None, callback=None):
+        res = {"url": url}
+        if message:
+            res["data"] = message
+        if callback:
+            client_id = time.time_ns()
+            res['client_id'] = client_id
+            callbacks[client_id] = callback
+        self.ws.send(json.dumps(res))
 
 
 class MessageController:
@@ -27,10 +51,16 @@ class MessageController:
     def create(self, data):
         print(data)
 
+    @staticmethod
+    def check(self, data):
+        return 'hello, bro!'
+
 
 connector = ClientConnector('localhost', 8765, {
     "/messages/create": MessageController.create,
+    "check": MessageController.check
 })
 
 while True:
-    connector.send('/messages/create', input())
+    input()
+    connector.send('/messages/get', callback=lambda self, x: print(x))
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+import time
+from typing import List, Tuple, Dict
+from collections.abc import Callable
+
 try:
     import thread
 except ImportError:
@@ -9,6 +13,7 @@ import tornado.ioloop
 import json
 import asyncio
 
+callbacks: Dict[int, Callable] = {}
 clients = []
 
 
@@ -18,9 +23,14 @@ class MessageController:
         for client in [c for c in clients if c != self]:
             client.send('/messages/create', data)
 
+    @staticmethod
+    def get(self, data):
+        return "this is data"
+
 
 routes = {
-    "/messages/create": MessageController.create
+    "/messages/create": MessageController.create,
+    "/messages/get": MessageController.get
 }
 
 
@@ -29,14 +39,32 @@ class ServerConnector(tornado.websocket.WebSocketHandler, ABC):
         clients.append(self)
         print("WebSocket opened")
 
-    def on_message(self, message):
+    def on_message(self, message: str):
         print(message)
-        message = json.loads(message)
-        if message['url'] and message['data'] and routes[message['url']]:
-            routes[message['url']](self, message['data'])
+        message: Dict = json.loads(message)
+        server_id = message.get('server_id')
+        client_id = message.get('client_id')
+        url = message.get('url')
+        if url and client_id:
+            handler = routes.get(url)
+            res = handler(self, message.get('data')) or None
+            self.write_message(json.dumps({"client_id": client_id, "data": res}))
+        elif url:
+            handler = routes.get(url)
+            handler(self, message.get('data'))
+        elif server_id:
+            handler = callbacks.pop(server_id)
+            handler(self, message.get('data'))
 
-    def send(self, url, msg):
-        self.write_message(json.dumps({"url": url, "data": msg}))
+    def send(self, url, message=None, callback=None):
+        res = {"url": url}
+        if message:
+            res["data"] = message
+        if callback:
+            server_id = time.time_ns()
+            res['server_id'] = server_id
+            callbacks[server_id] = callback
+        self.write_message(json.dumps(res))
 
     def on_close(self):
         clients.remove(self)
@@ -58,4 +86,4 @@ thread.start_new_thread(initServer, ())
 
 while True:
     msg = input()
-    clients[0].send('/messages/create', msg)
+    clients[0].send('check', callback=lambda self, x: print(x))
